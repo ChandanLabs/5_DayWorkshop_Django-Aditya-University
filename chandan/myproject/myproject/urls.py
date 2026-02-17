58652026473d8e46bd39374fe066533c164ef282
Chandansah_2005 <chandan.kumar.sah.teli2005@gmail.com>
2026-02-17 08:51:12 +0000

    additinal changes: render the html file using django templets

--- chandan/myproject/myproject/urls.py
+++ chandan/myproject/myproject/urls.py
@@ -20,6 +20,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("/",views.home, name='home'),
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('render/',views.home_view,name='home_view'),
+    
 ]
--- chandan/myproject/myproject/views.py
+++ chandan/myproject/myproject/views.py
@@ -1,2 +1,12 @@
-from django.http import HttpRequest
-form django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return HttpResponse("Welcome to Django")
+
+def home_view(request):
+    return render(request,'home.html')
+
+def about(request):
+    return HttpResponse("This is about path")
+
